d4eb54471bda66de6233a847032cf6ad19af9e29
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-29 12:00:51 +0200

    statemachine can now keep track of its states

--- src/nl/jappieklooster/ISAI/behaviour/state/StateMachine.groovy
+++ src/nl/jappieklooster/ISAI/behaviour/state/StateMachine.groovy
@@ -5,6 +5,28 @@ import nl.jappieklooster.ISAI.world.entity.MovingEntity;
 class StateMachine extends AbstractBehaviour{
 
 	IState currentState = null
+	
+	/**
+	 * used for identifying states with setcurrentstate "name"
+	 */
+	Map<String, IState> states
+	StateMachine(){
+		states = new HashMap<>()
+	}
+	void putAt(String key, IState value){
+		states[key]	= value
+	}
+	IState getAt(String key){
+		return states[key]
+	}
+	
+	void setCurrentState(String to){
+		IState state = states[to]
+		if(state == null){
+			throw new IllegalArgumentException("Could not find " + to + " state. make sure you named it exactly the same")
+		}
+		setCurrentState(state)
+	}
 
 	void setCurrentState(IState to){
 		if(currentState){
